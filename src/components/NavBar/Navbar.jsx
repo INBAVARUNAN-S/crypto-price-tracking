132e5e4589fa39b951b287b4bb7aae5a832a1326
INBAVARUNAN-S <inbavarunans.bcy24@rathinam.in>
2025-10-14 16:10:19 +0530

    updated

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
+import { name } from 'ejs'
 
 const Navbar = () => {
-    return (
+
+    const {setCurrency} = useContext(CoinContext);
+
+    const currencyHandler = (event) =>{
+        switch(event.target.value){
+            case "usd":{
+                setCurrency({name:"usd",symbol:"$"})
+                break;
+            }
+            case "eur":{
+                setCurrency({name:"eur",symbol:"€"})
+                break;
+            } 
+            case "inr":{
+                setCurrency({name:"inr",symbol:"₹"})
+                break;
+            }
+            default :{
+                setCurrency({name:"usd",symbol:"$"})
+                break;
+            }
+        }
+    }
+
+    return ( 
         <div className='navbar'>
-            <img src={logo} alt="" />
+            <img src={logo} alt="" className='logo'/> 
             <ul>
                 <li>Home</li>
                 <li>Features</li>
@@ -14,12 +40,12 @@ const Navbar = () => {
                 <li>Blog</li>
             </ul>
             <div className="nav-right">
-                <select>
+                <select onChange={currencyHandler}>
                     <option value="usd">USD</option>
                     <option value="eur">EUR</option> 
                     <option value="inr">INR</option>
                 </select>  
-                <button>Sign up <img src={arrow_icon} alt=""/></button>
+                <button>Sign up <img src={arrow_icon} className='btn-img' alt=""/></button>
             </div>
         </div>
     )
