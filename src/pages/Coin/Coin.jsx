b7d4a597198b28fd2fb54d27d92a9a321ea50778
INBAVARUNAN-S <inbavarunans.bcy24@rathinam.in>
2025-10-23 18:35:09 +0530

    updated

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -7,6 +7,7 @@ const Coin = () => {
 
   const { coinId } = useParams();
   const [coinData, setCoinData] = useState(null);
+  const [historicalData, setHistoricalData] = useState(null); 
   const { currency } = useContext(CoinContext);
 
   const fetchCoinData = async () => {
@@ -20,6 +21,20 @@ const Coin = () => {
     } catch (error) {
       console.error(error);
     }
+
+  }
+
+  const fetchHistoricalData = async () => {
+    const url = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`;
+    const options = { method: 'GET', headers: { 'x-cg-pro-api-key': 'CG-AoW8dEXe4ZW5YVNdTe46qa1H' }, body: undefined };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setHistoricalData(data);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
 
@@ -28,20 +43,19 @@ const Coin = () => {
   }, [currency])
 
 
-  if (coinData) {
+  if (coinData, historicalData) { 
     return (
       <div className='coin'>
-        <div className="coin-name"> 
-          <img src={coinData.image.large || "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400" } />
-          <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+        <div className="coin-name">
+          <img src={coinData?.image?.large || "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"} />
+          <p><b>{coinData?.name} ({coinData?.symbol?.toUpperCase()})</b></p>
         </div>
-      </div> 
-    )
+      </div>
+    ) 
   } else {
     return (
       <div className="spinner">
-        <div className="spin">
-
+        <div className="spin"> 
         </div>
       </div>
     )
